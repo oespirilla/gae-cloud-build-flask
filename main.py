84ab64dcb43665c33884789e3a80d0df137d58ea
Oscar Espirilla <oscar.espirilla@laureate.net>
2018-08-14 21:06:01 -0600

    testing cloud build triggers

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+    return 'Hello World from Cloud Build!'
 
 
 if __name__ == '__main__':
